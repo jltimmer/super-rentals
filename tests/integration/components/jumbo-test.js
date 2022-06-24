5479662063568159d2ac0dd596751676a3f8c682
Janna Timmer <jltimmer@users.noreply.github.com>
2022-06-23 20:45:54 -0400

    Add toggle button for image size and related tests

--- tests/integration/components/jumbo-test.js
+++ tests/integration/components/jumbo-test.js
@@ -8,7 +8,7 @@ module('Integration | Component | jumbo', function (hooks) {
 
   test('it renders the content inside a jumbo header with a tomster', async function (assert) {
     await render(hbs`<Jumbo>Hello World</Jumbo>`);
-    
+
     assert.dom('.jumbo').exists();
     assert.dom('.jumbo').hasText('Hello World');
     assert.dom('.jumbo .tomster').exists();

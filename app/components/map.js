32a2a3861548006030228335190143b33d5782cc
Janna Timmer <jltimmer@users.noreply.github.com>
2022-07-05 04:50:14 -0400

    Remove references to previous API and token

--- app/components/map.js
+++ app/components/map.js
@@ -1,20 +1,11 @@
 import Component from '@glimmer/component';
-import ENV from 'super-rentals/config/environment';
-
-const GOOGLE_API = 'https://maps.googleapis.com/maps/api/staticmap?'
 
 export default class MapComponent extends Component {
   get src() {
-    let { lat, lng, width, height, zoom } = this.args;
+    let { lat, lng, zoom } = this.args;
 
     let coordinates = `${lat},${lng}`;
-    let dimensions = `${width}x${height}`;
-    let accessToken = `key=${this.token}`;
 
-    return `${GOOGLE_API}&center=${coordinates}&zoom=${zoom}&size=${dimensions}&key=${accessToken}`
-  }
-
-  get token() {
-    return encodeURIComponent(ENV.GOOGLE_ACCESS_TOKEN);
+    return `https://maps.google.com/maps?q=${coordinates}&z=${zoom}&amp;output=embed`
   }
 }
